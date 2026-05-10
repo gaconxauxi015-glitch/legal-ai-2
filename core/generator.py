e071edf9830f48973e92f1324804157bfde6d5d3
gaconxauxi015-glitch <gaconxauxi015@gmail.com>
2026-05-10 11:23:21 +0700

    Create generator.py

---
+++ core/generator.py
@@ -0,0 +1,22 @@
+def generate_contract(desc):
+
+    return f"""
+HỢP ĐỒNG DỊCH VỤ
+
+ĐIỀU 1. THÔNG TIN
+
+Bên A: ..........
+Bên B: ..........
+
+ĐIỀU 2. NỘI DUNG
+
+{desc}
+
+ĐIỀU 3. THANH TOÁN
+
+- chuyển khoản
+
+ĐIỀU 4. CHẤM DỨT
+
+- theo thỏa thuận
+"""
